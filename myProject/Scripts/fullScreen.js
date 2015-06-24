fa427458290ba30b3e353dd8baf38fbd1437bf23
RaisaMeliksetyan <raisa.melliksetyan@gmail.com>
2015-06-24 22:08:28 +0400

    local commit

---
+++ myProject/Scripts/fullScreen.js
@@ -0,0 +1,38 @@
+﻿
+function fullScreen() {
+    var docElm = document.documentElement;
+    if (!full) {
+        
+        if (docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        }
+        else if (docElm.mozRequestFullScreen) {
+            docElm.mozRequestFullScreen();
+        }
+        else if (docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen(docElm.ALLOW_KEYBOARD_INPUT);
+        }
+        else if (docElm.msRequestFullscreen){
+            docElm.msRequestFullscreen();
+        }
+        full = 1;
+       
+        docElm.setAttribute('style', 'margin-left:0 !important');
+     //   docElm.css('margin-left', '24px');
+    }
+    else if (full) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+        else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        }
+        else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+        else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+        full = 0;
+    }
+}
